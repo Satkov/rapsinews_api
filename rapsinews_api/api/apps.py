b9e4eed46ef4893c697c3aa55f3e388e2cb3fa9f
Satkov <g.satkov@mail.ru>
2025-04-17 20:22:20 +0300

    add scheduler

--- rapsinews_api/api/apps.py
+++ rapsinews_api/api/apps.py
@@ -1,5 +1,11 @@
 from django.apps import AppConfig
+import os
 
-
-class NewsConfig(AppConfig):
+class ApiConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
     name = 'api'
+
+    def ready(self):
+        if os.environ.get('RUN_MAIN') == 'true':
+            from . import scheduler
+            scheduler.start()
--- rapsinews_api/rapsinews_api/settings.py
+++ rapsinews_api/rapsinews_api/settings.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -24,13 +25,14 @@ REST_FRAMEWORK = {
 }
 
 INSTALLED_APPS = [
-    'api.apps.NewsConfig',
+    'api.apps.ApiConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_apscheduler',
 ]
 
 MIDDLEWARE = [
