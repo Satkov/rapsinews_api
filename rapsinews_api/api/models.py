ff101556ae34052b153ebd980063af1a2ff18a49
Satkov <g.satkov@mail.ru>
2025-04-18 11:39:35 +0300

    add image fild to model

--- rapsinews_api/api/models.py
+++ rapsinews_api/api/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class Post(models.Model):
     title = models.CharField(max_length=255)
     link = models.URLField(unique=True)
+    image = models.URLField(blank=True, null=True)
     category = models.CharField(max_length=255)
     published = models.DateTimeField()
     full_text = models.TextField()
